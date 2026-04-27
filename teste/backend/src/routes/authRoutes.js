c94245f796e204929cd6073ca1bb8720d9616d31
Lorena Lira <naheelee2005@gmail.com>
2026-04-27 20:28:40 -0300

    ajustes

---
+++ teste/backend/src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/auth');
+
+router.post('/login', authController.login);
+router.post('/setup', authController.setup);
+router.post('/trocar-senha', authMiddleware(['student', 'coordinator', 'super_admin']), authController.trocarSenha);
+router.get('/me', authMiddleware(['student', 'coordinator', 'super_admin', 'admin']), authController.me);
+
+module.exports = router;
